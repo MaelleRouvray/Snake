003a1f71f1eacc47ceb7aac602ac9938d883ade0
Maelle Rouvray <maelle.rouvray@gmail.com>
2025-11-24 23:10:06 +0100

    ajout classe Snake + ajout snake.h et snake.cpp

---
+++ src/snake.cpp
@@ -0,0 +1,37 @@
+#include "snake.h"
+
+// constructeur (on initialise avec la tete)
+Snake::Snake(int x, int y) {
+    corps.push_back({x,y});
+    direction = {1,0};
+    grandit_apres = false;
+}
+
+
+// nouvelle direction
+void Snake::set_direction(int dx,int dy){
+    direction ={dx,dy};
+}
+
+// déplacement du Snake
+void Snake::deplace(){
+    std::pair<int,int> new_tete = {corps.front().first + direction.first,corps.front().second + direction.second};
+    corps.insert(corps.begin(),new_tete);
+    if (grandit_apres){
+        grandit_apres=false; // on garde la queue 
+    }
+    else{
+        corps.pop_back(); // on enlève la queue pour que corps ai la bonne taille après le déplacement
+    }
+}
+
+//vérifie collision du snake sur lui meme
+bool Snake::collision(){
+    std::pair<int,int> tete = corps.front();
+    for (size_t i=0;i < corps.size();i++){
+        if (corps[i]==tete){
+            return true;
+        }
+        return false;
+    }
+}
---
+++ src/snake.h
@@ -0,0 +1,31 @@
+#ifndef SNAKE_H
+#define SNAKE_H
+
+#include <iostream>
+#include <vector>
+#include <utility>
+using namespace std;
+
+class Snake {
+    private:
+    std::vector<std::pair<int,int>> corps; // vecteur où chaque composante est un couple (x,y)
+    std::pair<int,int> direction; // direction est un couple (dx,dy),
+    //                               directions possibles : (1,0):droite, (-1,0):gauche, (0,1):bas, (0,-1):haut
+    bool grandit_apres; // booléen qui indique si le serpent grandit au prochain move
+
+    public:
+    //constructeur:
+    Snake(int x,int y);
+
+    // déplacement
+    void set_direction(int dx,int dy);
+    void deplace();
+
+    //collision du snake sur lui même
+    bool collision();
+};
+
+
+
+
+#endif
