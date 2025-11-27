d06b6c19f66061a1134c77fbf41eb8cf7ad31a81
Maelle Rouvray <maelle.rouvray@gmail.com>
2025-11-27 16:17:32 +0100

    update main

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,11 @@
-int main(){
+// #include <SFML/Graphics.hpp>
+//#include < C:/Users/maell/SFML-3.0.2-windows-gcc-14.2.0-mingw-64-bit/SFML-3.0.2/include/SFML/Graphics.hpp>
+
+#include <iostream>
+#include "snake.h"
+#include "food.h"
+
+int main() {
 
     return 0;
 }
