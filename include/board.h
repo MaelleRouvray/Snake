4804f4064260351f1d6375613f14da23053efa35
Maelle Rouvray <maelle.rouvray@gmail.com>
2025-12-01 14:13:31 +0100

    add board

--- include/board.h
+++ include/board.h
@@ -9,9 +9,27 @@ using namespace std;
 
 class Board {
     private:
+    int length;
+    int width;
 
 
     public:
+    // constructeur
+    Board(int a=0, int b=0); 
+    
+    // accesseur
+    int get_length();
+    int get_width();
+
+    // affiche_plateau vide 
+    void show_board();
+
+    // ajoute snake sur le plateau 
+    void add_snake();
+
+    //ajoute food sur le plateau 
+    void add_food();
+
 };
 
 
---
+++ src/board.cpp
@@ -0,0 +1,27 @@
+#include "board.h"
+
+Board::Board(int a, int b) : length(a) , width(b) {}
+
+int Board::get_length(){
+    return length;
+}
+
+int Board::get_width(){
+    return width;
+}
+
+void Board::show_board(){
+    for (int i=0;i<width;i++){
+        cout << " ___" ;
+    };
+    cout << endl ;
+    for (int i=0 ; i<length ; i++){
+        for (int j=0 ; j<width ; j++){
+            cout << "|" ;
+            cout << "___" ;
+        }
+        cout << "|" ;
+        cout << endl;
+    };
+
+}
