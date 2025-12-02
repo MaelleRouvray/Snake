aa4fa5b882defc72e98b141f19fa1162c6620864
Coline Palefroy <colinepalefroy@gmail.com>
2025-12-02 16:42:33 +0100

    add exemple

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,35 @@
 #include <iostream>
 #include "snake.h"
 #include "food.h"
+#include "board.h"
+
 
 int main() {
+    Plateau plateau;
+    Snake snake (5,5);
+
+    Food food; 
+    food.generate_food(snake); 
+ 
+
+    initialisation_plateau(plateau);
+
+    dessine_snake(snake, plateau);
+    dessine_food(snake, food, plateau); 
+    affiche_plateau(plateau);
+
+    snake.set_direction(1,0); // vers la droite
+    snake.deplace();
+
+    dessine_snake(snake, plateau);
+    affiche_plateau(plateau);
+
+    snake.set_direction(1,0); // vers la droite
+    snake.deplace();
+
+    dessine_snake(snake, plateau);
+    affiche_plateau(plateau);
+
 
     return 0;
 }
