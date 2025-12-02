90e85c459ced122552b34e6c4c629aa2ad3f6df4
Coline Palefroy <colinepalefroy@gmail.com>
2025-12-02 22:15:58 +0100

    add example

--- src/main.cpp
+++ src/main.cpp
@@ -2,36 +2,42 @@
 //#include < C:/Users/maell/SFML-3.0.2-windows-gcc-14.2.0-mingw-64-bit/SFML-3.0.2/include/SFML/Graphics.hpp>
 
 #include <iostream>
+
 #include "snake.h"
 #include "food.h"
 #include "board.h"
+using namespace std; 
 
 
 int main() {
-    Plateau plateau;
+
+
+    Board board; 
+
     Snake snake (5,5);
 
     Food food; 
     food.generate_food(snake); 
  
 
-    initialisation_plateau(plateau);
+    board.initialisation_plateau();
 
-    dessine_snake(snake, plateau);
-    dessine_food(snake, food, plateau); 
-    affiche_plateau(plateau);
-
-    snake.set_direction(1,0); // vers la droite
-    snake.deplace();
-
-    dessine_snake(snake, plateau);
-    affiche_plateau(plateau);
+    
+    board.dessine_food(food); 
+    board.dessine_snake(snake);
+    board.affiche_plateau();
 
     snake.set_direction(1,0); // vers la droite
     snake.deplace();
 
-    dessine_snake(snake, plateau);
-    affiche_plateau(plateau);
+    board.dessine_snake(snake);
+    board.affiche_plateau();
+
+    snake.set_direction(1,0); // vers la droite
+    snake.deplace();
+
+    board.dessine_snake(snake);
+    board.affiche_plateau();
 
 
     return 0;
