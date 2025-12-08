85857b0c7028dabb7e8d0c44468499b8c986d427
Coline Palefroy <colinepalefroy@gmail.com>
2025-12-08 15:00:50 +0100

    edit food.cpp

--- src/food.cpp
+++ src/food.cpp
@@ -2,30 +2,31 @@
 #include "food.h"
 #include "snake.h"
 
+using namespace std; 
 
-std :: mt19937 gen(42);
+mt19937 gen(42);
 
 Food::Food(std::pair<int, int> a) : food(a) {}
 
-std::pair<int,int> Food::get_food() const{
+pair<int,int> Food::get_food() const{
     return food;
 }
 
-std::pair<int,int> Food::generate_food(const Snake& snake){
+pair<int,int> Food::generate_food(const Snake& snake){
 
     int width = 10; 
     int length =10; 
      
-    std::uniform_int_distribution<int> distX(0,width -1); 
-    std::uniform_int_distribution<int> distY(0,length -1); 
+    uniform_int_distribution<int> distX(0,width -1); 
+    uniform_int_distribution<int> distY(0,length -1); 
 
     int x = distX(gen);
     int y = distY(gen);
-    std::pair<int,int> food = {x,y}; 
+    pair<int,int> food = {x,y}; 
 
     //verifie que la nouriture n'est pas sur le snake//
     int i = 0;
-    std::vector<std::pair<int,int>> corps = snake.get_snake(); 
+    vector<pair<int,int>> corps = snake.get_snake(); 
 
     while (i < corps.size()){
         
@@ -33,7 +34,7 @@ std::pair<int,int> Food::generate_food(const Snake& snake){
             int x = distX(gen); 
             int y = distY(gen);  
             i=0; 
-            std::pair<int,int> food = {x,y}; 
+            pair<int,int> food = {x,y}; 
         }
         else{
             i++; 
@@ -44,7 +45,7 @@ std::pair<int,int> Food::generate_food(const Snake& snake){
 }
 
 
-std::pair<int,int> Food::new_food(const Snake& snake){
+pair<int,int> Food::new_food(const Snake& snake){
     food = generate_food(snake);
     // std::vector<std::pair<int,int>> corps = snake.get_snake(); 
     // if (food == corps.front()){
