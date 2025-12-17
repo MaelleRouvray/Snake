108f0104c1fc6b2b582dd2dc69342707665ac0eb
Maelle Rouvray <maelle.rouvray@gmail.com>
2025-12-17 14:06:06 +0100

    1ere version sfml

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 // #include <SFML/Graphics.hpp>
 // #include <C:/Users/maell/SFML-3.0.2-windows-gcc-14.2.0-mingw-64-bit/SFML-3.0.2/include/SFML/Graphics.hpp>
 
+
+///////////////////////
+///INUTILE ?         //
+///////////////////////
+
 // #include <iostream>
 
 // #include "snake.h"
@@ -48,29 +53,128 @@
 
 
 
-#include <thread>  
-#include <chrono>
+////////////////////////
+/// SANS SFML        ///
+////////////////////////
 
+// #include <thread>  
+// #include <chrono>
+
+// #include "snake.h"
+// #include "food.h"
+// #include "board.h"
+// #include "game.h"
+// using namespace std; 
+
+
+
+// int main() {
+
+//     Board board; 
+//     // board.initialisation_plateau();
+
+//     Snake snake (5,5);
+
+//     Food food; 
+//     food.new_food(snake);
+//     snake.set_direction(0,1); 
+
+//     run(snake, board, food); 
+
+//     return 0;
+// }
+
+
+#include <SFML/Graphics.hpp>
 #include "snake.h"
 #include "food.h"
 #include "board.h"
-#include "game.h"
 using namespace std; 
 
+int main(){
+    sf::RenderWindow window (sf::VideoMode({800,600}), "Snake avec SFML");  // grille 40x30
+    window.setFramerateLimit(60);
+    sf::Clock clock ;
+    float delay = 0.2f ;  // bouge toutes les 0.2s
+
+    Snake snake(2,15);
+    Food food ;
+    Board board ;
+
+    bool debut = false ;
+
+    while (window.isOpen()){
+        while (auto event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>()) {
+                window.close();
+            }
+
+            //entrées clavier
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left)){
+                snake.set_direction(-1,0);
+                debut = true ;
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right)){
+                snake.set_direction(1,0);
+                debut = true ;
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up)){
+                snake.set_direction(0,-1);
+                debut = true ;
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down)){
+                snake.set_direction(0,1);
+                debut = true ;
+            }
+
+        }
 
 
-int main() {
+        if (debut && clock.getElapsedTime().asSeconds() > delay){
+            if (snake.get_direction() != std::pair<int,int> (0,0)) {  //snake attend une commande clavier pour commencer a bouger
+                snake.deplace();
+            }
+            clock.restart(); 
 
-    Board board; 
-    // board.initialisation_plateau();
+            auto corps = snake.get_snake() ;
+            auto tete = corps.front();
+            auto coord_food = food.get_food();
 
-    Snake snake (5,5);
+            if(tete == coord_food){     // si serpent mange nourriture alors grandir puis new food
+                snake.grandir();
+                food.new_food(snake);
+            }
+        }
 
-    Food food; 
-    food.new_food(snake);
-    snake.set_direction(0,1); 
 
-    run(snake, board, food); 
 
-    return 0;
+        // rendu graphique
+        window.clear(sf::Color::Black);
+
+        // dessin serpent
+        auto corps = snake.get_snake() ;
+        for (size_t i = 0;i < corps.size(); i++){
+            sf::RectangleShape rect(sf::Vector2f(20, 20)); //taille d'uen case
+            rect.setPosition(sf::Vector2f(static_cast<float>(corps[i].first * 20), static_cast<float>(corps[i].second * 20)));
+            if (i == 0){
+                rect.setFillColor(sf::Color::Yellow) ; // tete du serpent
+            }
+            else {
+                rect.setFillColor(sf::Color::Green) ; // reste du corps
+            }
+            window.draw(rect) ;
+        }
+
+        // dessin food
+        auto coord_food = food.get_food();
+        sf::CircleShape food_circle(10.f);
+        food_circle.setPosition(sf::Vector2f(static_cast<float>(coord_food.first*20), static_cast<float>(coord_food.second*20)));
+        food_circle.setFillColor(sf::Color::Red);
+        window.draw(food_circle);
+
+
+        window.display();
+    }
+
+    return 0 ;
 }
